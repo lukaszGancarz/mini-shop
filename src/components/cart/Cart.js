a72ee49b6b3d63fdc7615d4e7023550d55369b7d
lukaszGancarz <lukasz.gancarz@wp.pl>
2021-07-24 21:50:09 +0200

    Cart has been implemented

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -5,13 +5,25 @@ const Cart = (props) => {
   return (
     <div className={styles.list}>
       <ul className={styles.ul}>
-        {props.items.map((item) => ( 
+        {props.items.map((item) => (
           <li className={styles.li}>
-            <div className={styles.photo}>{item.photo}</div>
+            <div className={styles.photoDiv}>
+              <img
+                className={styles.photo}
+                src={item.photo}
+                alt={"zdjęcie"}
+              ></img>
+            </div>
             <div className={styles.title}>{item.title}</div>
-            <div className={styles.desription}>{item.description}</div>
-            <div className={styles.count}>{item.count} x {item.price}</div>
-            <div className={styles.total}>{(item.count * item.price).toFixed(2)}</div>
+            <div className={styles.description}>{item.description}</div>
+            <div className={styles.price}>
+              {item.count} szt. x <br />
+              {item.price.toFixed(2)} zł.
+            </div>
+            <div className={styles.equal}>=</div>
+            <div className={styles.total}>
+              {(item.count * item.price).toFixed(2)} zł
+            </div>
           </li>
         ))}
       </ul>
